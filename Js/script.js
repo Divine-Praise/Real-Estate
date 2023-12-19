fb693e59b357e73198b67f0c9eecb72e6debf618
Divine-Praise <divineatansi123@gmail.com>
2023-12-19 04:59:34 -0800

    new commit

--- Js/script.js
+++ Js/script.js
@@ -29,3 +29,29 @@
               },
             },
           });
+
+
+//about dropdown
+const icone1 = document.getElementById('close1');
+const icone2 = document.getElementById('close2');
+const icone3 = document.getElementById('close3');
+const content1 = document.querySelector('#text1');
+const content2 = document.querySelector('#text2');
+const content3 = document.querySelector('#text3');
+
+// functions
+  icone1.addEventListener('click', function(){
+    content1.classList.toggle("active2");
+    content2.classList.remove("active2");
+    content3.classList.remove("active2");
+  });
+  icone2.addEventListener('click', function(){
+    content1.classList.remove("active2");
+    content2.classList.toggle("active2");
+    content3.classList.remove("active2");
+  });
+  icone3.addEventListener('click', function(){
+    content1.classList.remove("active2");
+    content2.classList.remove("active2");
+    content3.classList.toggle("active2");
+  });
